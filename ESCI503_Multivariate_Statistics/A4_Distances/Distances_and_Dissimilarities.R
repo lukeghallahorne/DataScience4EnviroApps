46c511abe9a3b981bf24dc6213585d752ccb02fa
Luke Ghallahorne <161077615+lukeghallahorne@users.noreply.github.com>
2025-05-21 08:16:40 -0700

    Seminar and Course input
    Updated repository to include all work from Data Science for Environmental Applications seminars and courses

---
+++ ESCI503_Multivariate_Statistics/A4_Distances/Distances_and_Dissimilarities.R
@@ -0,0 +1,7 @@
+flower <- matrix(data = c(0, 0, 1, 2, 2,
+                          1, 3, 2, 0, 1,
+                          2, 1, 0, 4, 4),
+                 nrow = 5, ncol = 3)
+flower
+
+dist(flower)
